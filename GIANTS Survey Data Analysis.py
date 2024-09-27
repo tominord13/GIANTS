898f096b13fc31de8d8da55d2ec9b1e297c42f2d
Tomi <nord.tomi@gmail.com>
2024-09-27 10:14:39 +0200

    Deleted unnecessary files

--- GIANTS Survey Data Analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib
-import os
-
-#print(os.getcwd())  
-#print(os.listdir())
-#os.chdir('c:/Users/TomiNordi2m/OneDrive - i2m Unternehmensentwicklung GmbH/Documents/Python/GIANTS Survey Data Analysis')
-#Load the Excel file
-#df = pd.read_excel('2024.08.08 GIANTS project_ Survey Response Data.xlsx')
